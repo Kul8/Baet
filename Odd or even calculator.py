a3a21f7149609b703d6e344f34f34cf32a190d1a
Kul8 <93385979+Kul8@users.noreply.github.com>
2022-03-31 15:42:16 +0800

    Update Odd or even calculator.py

--- Odd or even calculator.py
+++ Odd or even calculator.py
@@ -1,6 +1,6 @@
 print("| This program will be distinguishing \n| \" between odd or even numbers\"")
 number = input("| Please enter a number to be destinguished " )
-if number % 2 == 0:
+if number % (2) == 0:
     print("| even")
 else:
     print("| Odd")
