9115f48d75714f6e2ab6cb88f9c387ba4e433c48
Kul8 <jrbsibayan@iskolarngbayan.pup.edu.ph>
2022-03-31 15:25:52 +0800

    Wala lang

---
+++ Odd or even calculator.py
@@ -0,0 +1,9 @@
+print("| This program will be distinguishing \n| \" between odd or even numbers\"")
+number = input("| Please enter a number to be destinguished " )
+if number % 2 == 0:
+    print("| even")
+else:
+    print("| Odd")
+
+
+
